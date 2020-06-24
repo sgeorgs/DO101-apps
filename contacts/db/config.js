b1db0a1abf247e31e4cc51be4afaeedd60640650
sgeorgs <g.stalla@chello.at>
2020-06-24 12:08:25 +0200

    ww troubles

--- contacts/db/config.js
+++ contacts/db/config.js
@@ -1,10 +1,10 @@
 require('dotenv').config();
 const DB_CONFIG = process.env.DB_CONFIG || 'postgresql://contacts:contacts@localhost:5432/contacts';
-const { Pool } = require('pg');
+import { Pool } from 'pg';
 
 const pgconn = new Pool({
     connectionString: DB_CONFIG,
     ssl: false,
 });
-  
-module.exports = { pgconn }
+
+export default { pgconn }
